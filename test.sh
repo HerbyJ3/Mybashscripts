4329d88a6f2e38f3bfd945a1e8f81f80793e2b77
Herby <jeanty.herby@gmail.com>
2022-05-31 19:04:09 +0000

    Just moved this script file over

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Please input a new user below:"
+read -p 'Username: ' name
+echo "Checking existing usernames...................Please wait"
+sleep 3
+while true; do
+	if getent passwd $name > /dev/null; then
+		echo "This user already exists, please re-enter a new user"
+		read -p 'Username: ' name
+	else
+		sudo useradd -m $name
+		echo "Processing new user.........................Please wait"
+		sleep 3
+		echo $name "has been added as a new usename"
+		break
+	fi
+done
